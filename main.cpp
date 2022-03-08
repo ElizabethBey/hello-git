dbafb57ab93d3c2d4d84ea39df37b58d3fc91322
ElizabethBey <elizbey@yandex.ru>
2022-03-08 22:20:36 +0100

    Добавила ркурсивную функцию fib

--- main.cpp
+++ main.cpp
@@ -2,8 +2,17 @@
 
 using namespace std;
 
+int fib(int n)
+{
+    if (n <= 2)
+        return 1;
+    else
+        return fib(n - 1) + fib(n - 2);
+}
+
 int main()
 {
     cout << "Hello, world!" << endl;
+    cout << "fib(10) = " << fib(10) << endl;
     return 0;
 }

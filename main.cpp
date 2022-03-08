ead56ead195c9350575e70919f5ada4ea068210e
ElizabethBey <elizbey@yandex.ru>
2022-03-08 22:23:20 +0100

    Исправила функцию fib

--- main.cpp
+++ main.cpp
@@ -4,7 +4,9 @@ using namespace std;
 
 int fib(int n)
 {
-    if (n <= 2)
+    if (n == 1)
+        return 0;
+    else if (n == 2)
         return 1;
     else
         return fib(n - 1) + fib(n - 2);
